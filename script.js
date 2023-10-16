58110dd4415aef7b7373b4d4e30be643b471507d
vishwas <vishwasbabar886@gmail.com>
2023-10-16 07:17:26 +0530

    added the cross line on tasks when we toggle checkbox

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addTask() {
     const listItem = document.createElement("li");
     listItem.className = "todo-item";
     listItem.innerHTML = `
-                    <input type="checkbox">
+                    <input type="checkbox" class = "checkbox">
                     <span>${taskText}</span>
                     <button class="delete-button">Delete</button>
                 `;
@@ -23,5 +23,8 @@ function addTask() {
 todoList.addEventListener("click", (event) => {
   if (event.target.className === "delete-button") {
     event.target.parentElement.remove();
+  }else if(event.target.className === "checkbox") {
+    const tasks = event.target.parentElement;
+    tasks.classList.toggle("completed")
   }
 });
